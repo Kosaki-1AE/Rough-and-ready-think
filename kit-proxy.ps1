fa77013049d0c515fdb66a51bcb59e13d5125875
Kosaki-1AE <kos0430@icloud.com>
2026-08-24 13:53:52 +0900

    vault backup: 2026-08-24 13:53:52

--- kit-proxy.ps1
+++ kit-proxy.ps1
@@ -3,20 +3,20 @@ $ssid = (netsh wlan show interfaces |
     Select-Object -First 1).ToString().Split(':',2)[1].Trim()
 
 if ($ssid -eq "KIT-WLAP2") {
-    $env:http_proxy  = "http://wwwproxy1.kanazawa-it.ac.jp:8080"
-    $env:https_proxy = "http://wwwproxy1.kanazawa-it.ac.jp:8080"
-
-    git config --global http.proxy  http://wwwproxy1.kanazawa-it.ac.jp:8080
-    git config --global https.proxy http://wwwproxy1.kanazawa-it.ac.jp:8080
-
-    Write-Host "KITinside: Proxy ON"
-}
-else {
     Remove-Item Env:http_proxy  -ErrorAction SilentlyContinue
     Remove-Item Env:https_proxy -ErrorAction SilentlyContinue
 
     git config --global --unset http.proxy  2>$null
     git config --global --unset https.proxy 2>$null
 
-    Write-Host "KIToutside: Proxy OFF"
+    Write-Host "KITinside : Proxy OFF"
+}
+else {
+    $env:http_proxy  = "http://wwwproxy1.kanazawa-it.ac.jp:8080"
+    $env:https_proxy = "http://wwwproxy1.kanazawa-it.ac.jp:8080"
+
+    git config --global http.proxy  http://wwwproxy1.kanazawa-it.ac.jp:8080
+    git config --global https.proxy http://wwwproxy1.kanazawa-it.ac.jp:8080
+
+    Write-Host "KIToutside : Proxy ON"
 }
