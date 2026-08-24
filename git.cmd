6332d70904813ca8e52d0ee44b804ce613b01453
Kosaki-1AE <kos0430@icloud.com>
2026-08-24 14:01:42 +0900

    vault backup: 2026-08-24 14:01:42

--- git.cmd
+++ git.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\path\to\kit-proxy.ps1"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\Users\kos04\Documents\KQI\kit-proxy.ps1"
 "C:\Program Files\Git\cmd\git.exe" %*
